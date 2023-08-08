9058de0662228efd953f0e05131d394b9a39211a
LAPTOP-2P239E4A\hacon <hacondesign@gmail.com>
2023-08-08 06:17:57 -0400

    binding

--- src/app/modules/admin/components/home/home.component.ts
+++ src/app/modules/admin/components/home/home.component.ts
@@ -8,8 +8,18 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   constructor() { }
+  change: any = Number;
+  amounts = {
+    totalAmount: 33,
+    billNPay: 400000,
+    totalSpend: 20000000,
+    totalIncome: 39999999,
+    totalExpense: 6688385868,
+    totalBilling: 7098958483737
+  }
 
   ngOnInit(): void {
+
   }
 
 }
